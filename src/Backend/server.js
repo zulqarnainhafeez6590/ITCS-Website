4ebebffd2ae03ea13ed9efbca121d095e0d438eb
Abdul Raheem <abdul.raheem.17.9.2002@gmail.com>
2026-04-23 16:25:16 +0500

    Configure for Vercel deployment

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -35,4 +35,8 @@ mongoose
 
 // Start server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
+
+export default app
